442aa60f49f2e3a2c9f26a322b110e2142bd49d7
javzero <javzero@hotmail.com>
2017-09-28 03:56:39 -0300

    Seo Configuration

--- resources/views/web/layouts/main.blade.php
+++ resources/views/web/layouts/main.blade.php
@@ -21,19 +21,24 @@
 		<!-- Favicon -->
 		<link rel="shortcut icon" href="{{ asset('webimages/logos/favicon.png') }}">
 
-		<meta property="og:url"         content="http://studiovimana.com.ar" />
-		<meta property="og:type"        content="article" />
-		<meta property="og:title"       content="Diseño Web y Diseño Gráfico" />
+		<meta property="og:url" content="http://vimana.studio" />
+		<meta property="og:type" content="article" />
+		<meta property="og:title" content="Diseño Web y Diseño Gráfico" />
 		<meta property="og:description" content="Somos un equipo de trabajo dedicado a desarrollar contenido visual e interactivo" />
-		<meta property="og:image"       content="{{ asset('webimages/logos/main-logo.png') }}" />
-		<meta name="twitter:title"      content="Studio Vimana" />
-		<meta name="twitter:image"      content="{{ asset('webimages/logos/main-logo.png') }}" />
-		<meta name="twitter:url"        content="http://studiovimana.com.ar" />
+		<meta property="og:image" content="{{ asset('webimages/logos/main-logo.png') }}" alt="Studio Vimana Logo"/>
+		<meta property="og:site_name" content="http://vimana.studio">
+		<meta property="og:locale" content="es_ES">
+		<meta property="fb:app_id" content="1305647396212353">
+
+
+		<meta name="twitter:title" content="Studio Vimana" />
+		<meta name="twitter:image" content="{{ asset('webimages/logos/main-logo.png') }}" alt="Studio Vimana Logo"/>
+		<meta name="twitter:url"content="http://vimana.studio" />
 		{{-- <meta name="twitter:card"       content="" /> --}}
 		{{--<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">--}}
-		<link rel="stylesheet" async  type="text/css" href="{{ asset('plugins/bootstrap/css/bootstrap.min.css') }}">
-		<link rel="stylesheet" async  type="text/css" href="{{ asset('plugins/animate/animate.css') }}">
-		<link rel="stylesheet" async  type="text/css" href="{{ asset('plugins/ionicons/ionicons.min.css') }}"> 
+		<link rel="stylesheet" type="text/css" href="{{ asset('plugins/bootstrap/css/bootstrap.min.css') }}">
+		<link rel="stylesheet" type="text/css" href="{{ asset('plugins/animate/animate.css') }}">
+		<link rel="stylesheet" type="text/css" href="{{ asset('plugins/ionicons/ionicons.min.css') }}"> 
 		@yield('styles')
 		<link rel="stylesheet" type="text/css" href="{{ asset('css/web.css') }}">
 
