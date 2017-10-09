3eabb14cf430dfb76163fa5109dabb3b5d0f7c52
javzero <javzero@hotmail.com>
2017-10-09 00:13:44 -0300

    Testing https

--- resources/views/layouts/web/partials/scripts.blade.php
+++ resources/views/layouts/web/partials/scripts.blade.php
@@ -1,5 +1,5 @@
-<script type="text/javascript" src="{{asset('plugins/jquery/jquery-3.3.1.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('plugins/bootstrap/tether.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('plugins/bootstrap/bootstrap3/bootstrap.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('plugins/wow/script.wow.js')}}"></script>
-<script type="text/javascript" src="{{ asset('js/web.js') }}"></script>
+<script type="text/javascript" src="{{secure_asset('plugins/jquery/jquery-3.3.1.min.js')}}"></script>
+<script type="text/javascript" src="{{secure_asset('plugins/bootstrap/tether.min.js')}}"></script>
+<script type="text/javascript" src="{{secure_asset('plugins/bootstrap/bootstrap3/bootstrap.min.js')}}"></script>
+<script type="text/javascript" src="{{secure_asset('plugins/wow/script.wow.js')}}"></script>
+<script type="text/javascript" src="{{ secure_asset('js/web.js') }}"></script>
