e73522a7424f304ddf59b3a41151510464c74629
javzero <javzero@hotmail.com>
2017-12-12 02:46:30 -0300

    Web Front Redesign

--- resources/views/layouts/web/partials/contact-tab.blade.php
+++ resources/views/layouts/web/partials/contact-tab.blade.php
@@ -1,3 +1,3 @@
 <div class="contact-tab">
-    <a href="https://api.whatsapp.com/send?phone=51601565"><img src="{{ asset('webimages/gral/icons/whatsapplogo.png') }}" alt=""></a>   
+    <a href="https://api.whatsapp.com/send?phone=1551601565"><img src="{{ asset('webimages/gral/icons/whatsapplogo.png') }}" alt=""></a>   
 </div>
