c3bed9855c35698a2ee4e6de37679f04ad959dd7
jav-0 <javzero1@gmail.com>
2018-01-11 00:26:05 -0300

    Switch

--- app/Http/Controllers/Store/CartDetailController.php
+++ app/Http/Controllers/Store/CartDetailController.php
@@ -11,7 +11,7 @@ class CartDetailController extends Controller
     public function store(Request $request)
     {
         $cartDetail = new CartDetail();
-        dd(auth()->customer()->carts());
+        dd(auth()->guard('customer')->user()->carts());
         $cartDetail->cart_id = auth()->guard('customer')->user()->cart_id;
         $cartDetail->product_id = $request->product_id;
         $cartDetail->quantity = $request->quantity;
